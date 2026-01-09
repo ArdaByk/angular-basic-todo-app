f8931efe9c9c1a16e576c30ce8a54d1107dc6ece
Arda <ardabuyuk2738@gmail.com>
2026-01-09 18:51:48 +0300

    The program created.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,45 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('todo-app');
+
+  todo:string = ""
+  updatedTodo:string = ""
+  updatedTodoIndex:number = 0
+  deletedTodoIndex:number = 0
+  todos:string[] = []
+  isUpdateModeActive:boolean = false
+
+  saveTodo(){
+    this.todos.push(this.todo)
+
+    this.todo = ""
+  }
+
+  updateTodo(){
+
+    this.todos[this.updatedTodoIndex] = this.updatedTodo
+    this.isUpdateModeActive = false
+
+  }
+
+  getTodo(index:any){
+    this.isUpdateModeActive = true
+    this.updatedTodo = this.todos[index]
+  }
+
+  deleteTodo(index:any){
+    this.todos.splice(index, 1)
+  }
+
+  deleteAllTodos(){
+    this.todos = []
+  }
+
 }
